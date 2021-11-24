4dbe1d1abe2ca6b81f12bf6198dea8ac9170e5f0
ivanilsonsilva <ivanilsonsilva249@gmail.com>
2021-11-24 15:19:50 -0300

    first commit

---
+++ usuarios/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import usuarios
+from .models import Usuario
+from django.shortcuts import redirect
+import hashlib
+
+
+
+def cadastro(request):
+    if request.session.get('usuario'):
+        return redirect('/home')
+    status = request.GET.get('status')
+
+    return render(request,'cadastro.html',{'status': status})
+
+
+
+def login(request):
+    if request.session.get('usuario'):
+        return redirect('/home')
+    status = request.GET.get('status')
+    return render(request,'login.html',{'status': status})
+
+
+def valida_cadastro(request):
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    
+    usuario = Usuario.objects.filter(email = email)
+
+
+    if len(senha) < 8:
+        return redirect('/auth/cadastro/?status=1')
+
+    
+    if len(nome.strip()) == 0 or len(email.strip()) == 0:
+        return redirect('/auth/cadastro/?status=2')
+
+    if len(usuario) > 0:
+        return redirect('/auth/cadastro/?status=3')
+   
+    
+    try:
+        senha = hashlib.sha256(senha.encode()).hexdigest()
+        usuario = Usuario(nome = nome,
+                          email = email,
+                          senha = senha)
+        usuario.save()
+        return redirect('/auth/cadastro/?status=0')
+    except:
+        return redirect(' ERRO INTERNO SO SISTEMA, TENTE NOVAMENTE EM INSTANTES')
+
+def valida_login(request):
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    senha = hashlib.sha256(senha.encode()).hexdigest()
+    usuarios = Usuario.objects.filter(email = email).filter(senha = senha)
+
+    if len(usuarios) == 0:
+        return redirect('/auth/login?status=1')
+    elif len(usuarios)> 0:
+        request.session['usuario'] = usuarios[0].id
+        return redirect('/home')
+def sair(request):
+    request.session.flush()
+    return redirect('/auth/login')  
+
+
+
+
+
+
+
+
+
+
+# Create your views here.
